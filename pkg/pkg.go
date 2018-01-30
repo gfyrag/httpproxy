dc282ade2f558ab7fee947c3221314372981de60
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2018-01-30 16:50:18 +0100

    Handle CONNECT method

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -3,6 +3,9 @@ package httpproxy
 import (
 	"net/http"
 	"net/url"
+	"net"
+	"time"
+	"io"
 )
 
 var (
@@ -14,38 +17,72 @@ var (
 	}
 )
 
+func handleTunneling(w http.ResponseWriter, r *http.Request) {
+
+	remoteConn, err := net.DialTimeout("tcp", r.Host, 10 * time.Second)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer remoteConn.Close()
+	w.WriteHeader(http.StatusOK)
+
+	hi, ok := w.(http.Hijacker)
+	if !ok {
+		panic("conn can't be hijacked")
+	}
+
+	clientConn, _, err := hi.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer clientConn.Close()
+
+	go io.Copy(remoteConn, clientConn)
+	io.Copy(clientConn, remoteConn)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	hi, ok := w.(http.Hijacker)
+	if !ok {
+		panic("conn can't be hijacked")
+	}
+
+	uri, err := url.Parse(r.RequestURI)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+
+	r.URL = uri
+	r.RequestURI = ""
+
+	resp, err := httpClient.Do(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+
+	clientConn, _, err := hi.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer clientConn.Close()
+
+	err = resp.Write(clientConn)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func Handler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		hi, ok := w.(http.Hijacker)
-		if !ok {
-			panic("conn can't be hijacked")
-		}
-
-		conn, _, err := hi.Hijack()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusServiceUnavailable)
-			return
-		}
-
-		uri, err := url.Parse(r.RequestURI)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusServiceUnavailable)
-			return
-		}
-
-		r.URL = uri
-		r.RequestURI = ""
-
-		resp, err := httpClient.Do(r)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusServiceUnavailable)
-			return
-		}
-
-		err = resp.Write(conn)
-		if err != nil {
-			panic(err)
+		if r.Method == http.MethodConnect {
+			handleTunneling(w, r)
+		} else {
+			handleRequest(w, r)
 		}
 	})
 }
--- pkg/pkg_test.go
+++ pkg/pkg_test.go
@@ -6,14 +6,17 @@ import (
 	"net/http/httptest"
 	"net/http"
 	"net/url"
+	"crypto/tls"
 )
 
 type HTTPProxyTestSuite struct {
 	suite.Suite
 }
 
-func (s *HTTPProxyTestSuite) TestHandler() {
+func (s *HTTPProxyTestSuite) TestHTTP() {
 	srv := httptest.NewServer(Handler())
+	defer srv.Close()
+
 	proxyUrl, err := url.Parse(srv.URL)
 	s.NoError(err)
 	client := &http.Client{
@@ -29,6 +32,33 @@ func (s *HTTPProxyTestSuite) TestHandler() {
 	s.NoError(err)
 
 	rsp, err := client.Do(req)
+	s.NoError(err)
+	s.NotNil(rsp)
+	s.Equal(http.StatusMovedPermanently, rsp.StatusCode)
+}
+
+func (s *HTTPProxyTestSuite) TestHTTPS() {
+	srv := httptest.NewServer(Handler())
+	defer srv.Close()
+
+	proxyUrl, err := url.Parse(srv.URL)
+	s.NoError(err)
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+			DisableCompression: true,
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+			TLSNextProto:    make(map[string]func(string, *tls.Conn) http.RoundTripper),
+		},
+	}
+
+	rsp, err := client.Get("https://google.fr")
 	s.NoError(err)
 	s.NotNil(rsp)
 	s.Equal(http.StatusMovedPermanently, rsp.StatusCode)
