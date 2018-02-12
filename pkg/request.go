0cb43bf3f1d5828c4b21d1065970d04baf23ba31
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2018-02-12 20:25:00 +0100

    Better logging and fix some potential bugs.

--- pkg/request.go
+++ pkg/request.go
@@ -12,6 +12,7 @@ import (
 	"io"
 	"fmt"
 	"time"
+	"github.com/pkg/errors"
 )
 
 type dialer func(context.Context) error
@@ -40,7 +41,7 @@ func (r *Request) dialRemote() error {
 func (r *Request) doRequest(req *http.Request) (*http.Response, error) {
 	err := r.dialRemote()
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "dial remote")
 	}
 
 	if r.logger.Level <= logrus.DebugLevel {
@@ -50,11 +51,11 @@ func (r *Request) doRequest(req *http.Request) (*http.Response, error) {
 
 	err = req.Write(r.remoteConn)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "write request to remote")
 	}
 	resp, err := http.ReadResponse(bufio.NewReader(r.remoteConn), req)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "read response from remote")
 	}
 
 	if r.logger.Level <= logrus.DebugLevel {
@@ -72,13 +73,19 @@ func (r *Request) checkModified(cachedResponse *http.Response, req *http.Request
 	}
 	rsp, err := r.doRequest(req)
 	if err != nil {
-		return nil, false, err
+		return nil, false, errors.Wrap(err, "check modified request")
 	}
 	if rsp.StatusCode == http.StatusNotModified {
 		rsp.Body.Close()
 		return rsp, false, nil
 	}
 	return rsp, true, nil
+}
+
+func (r *Request) isCacheAware(req *http.Request) bool {
+	return req.Header.Get("If-None-Match") != "" ||
+		req.Header.Get("If-Match") != "" ||
+		req.Header.Get("If-Modified-Since") != ""
 }
 
 func (r *Request) handleRequest(req *http.Request) error {
@@ -101,32 +108,36 @@ func (r *Request) handleRequest(req *http.Request) error {
 
 	meta, err := r.tx.ReadMetadata()
 	if err != nil && err != ErrCacheMiss {
-		return err
+		return errors.Wrap(err, "reading cache")
 	}
 
 	if err == nil {
 		cachedResponse, err = r.tx.ReadResponse()
 		if err != nil {
-			panic(err)
+			return errors.Wrap(err, fmt.Sprintf("reading cached response %s", r.tx.id))
 		}
 		if meta.Expired() {
 			r.logger.Debugf("found expired response in cache (%s)", meta.Expires)
-			var modified bool
-			rsp, modified, err = r.checkModified(cachedResponse, req)
-			if err != nil {
-				return err
-			}
-			if modified {
-				r.logger.Debugf("server respond with modified content, close the cached response and issue new request")
-				cachedResponse.Body.Close()
-			} else {
-				r.logger.Debugf("server respond with not modified content (304), update meta and respond with cached response")
-				rsp = cachedResponse
-				meta, err = r.tx.Prepare(rsp)
+			if !r.isCacheAware(req) {
+				r.logger.Debugf("request does not contains cache directive")
+				var modified bool
+				rsp, modified, err = r.checkModified(cachedResponse, req)
 				if err != nil {
-					r.logger.Error("error while writing metadata: %s", err)
-					return err
+					return errors.Wrap(err, "checking if response was modified")
 				}
+				if modified {
+					r.logger.Debugf("server respond with modified content, close the cached response and issue new request")
+					cachedResponse.Body.Close()
+				} else {
+					r.logger.Debugf("server respond with not modified content (304), update meta and respond with cached response")
+					rsp = cachedResponse
+					meta, err = r.tx.Prepare(rsp)
+					if err != nil {
+						return errors.Wrap(err, "updating metadata")
+					}
+				}
+			} else {
+				r.logger.Debugf("request contains cache directive, pass through")
 			}
 		} else {
 			r.logger.Debugf("found fresh response in cache (%s), serve cached response", meta.Expires)
@@ -138,7 +149,7 @@ func (r *Request) handleRequest(req *http.Request) error {
 		r.logger.Debugf("cache miss, issue new request")
 		rsp, err = r.doRequest(req)
 		if err != nil {
-			return err
+			return errors.Wrap(err, "performing request")
 		}
 	}
 
@@ -156,20 +167,28 @@ func (r *Request) handleRequest(req *http.Request) error {
 
 		meta, err := r.tx.Prepare(rspCp)
 		if err != nil {
-			r.logger.Error("error while writing metadata: %s", err)
-			return err
+			return errors.Wrap(err, "writing metadata")
 		}
 		r.logger.Debugf("cache response, will expires at %s", meta.Expires)
 		go func() {
 			err = r.tx.WriteResponse(rspCp)
 			if err != nil {
-				r.logger.Error("error while caching response: %s", err)
+				r.logger.Error("caching response: %s", err)
 				return
 			}
 		}()
 	}
 
-	return rsp.Write(r.clientConn)
+	err = rsp.Write(r.clientConn)
+	if err != nil {
+		// Force the complete read of the body to complete caching operation
+		_, err := io.Copy(ioutil.Discard, rsp.Body)
+		if err != nil {
+			return errors.Wrap(err, "flushing response")
+		}
+		return errors.Wrap(err, "writing response")
+	}
+	return nil
 }
 
 func (r *Request) writeStatusLine(status int, text string) error {
